3e5142350d7451c64991fb311ccd89908c6bf53f
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-08-26 10:15:41 +0100

    Add icon barrel for SVG imports as React components
    - Created a new `index.ts` file in the `src/icons` directory to export multiple SVG icons as React components.
    - Removed the `Icon?` entry from `.gitignore` to allow for proper SVG imports.

---
+++ src/icons/index.ts
@@ -0,0 +1,48 @@
+// Icon barrel importing SVGs as React components using ?react query
+// Usage: import { IconSearch } from '../icons'; <IconSearch />
+
+import IconFeedC from '../svg/iconFeed.svg?react';
+import IconDiamondC from '../svg/iconDiamond.svg?react';
+import IconPostsC from '../svg/iconPosts.svg?react';
+import IconHashtagC from '../svg/iconHashtag.svg?react';
+import IconWalletC from '../svg/iconWallet.svg?react';
+import IconSearchC from '../svg/iconSearch.svg?react';
+import IconMobileMenuC from '../svg/iconMobileMenu.svg?react';
+import IconThreeDotsC from '../svg/iconThreeDots.svg?react';
+import IconPlusC from '../svg/iconPlus.svg?react';
+import IconUserC from '../svg/iconUser.svg?react';
+import IconReplyC from '../svg/iconReply.svg?react';
+import IconShareC from '../svg/iconShare.svg?react';
+import IconFilterC from '../svg/iconFilter.svg?react';
+import IconSortC from '../svg/iconSort.svg?react';
+import HeaderLogoC from '../svg/headerLogo.svg?react';
+import IconCommentC from '../svg/iconComment.svg?react';
+import IconPicturesC from '../svg/iconPictures.svg?react';
+import ExternalLinkC from '../svg/externalLink.svg?react';
+import IconCloseC from '../svg/iconClose.svg?react';
+import IconSmileC from '../svg/iconSmile.svg?react';
+import IconGifC from '../svg/iconGif.svg?react';
+
+export const IconFeed = IconFeedC;
+export const IconDiamond = IconDiamondC;
+export const IconPosts = IconPostsC;
+export const IconHashtag = IconHashtagC;
+export const IconWallet = IconWalletC;
+export const IconSearch = IconSearchC;
+export const IconMobileMenu = IconMobileMenuC;
+export const IconThreeDots = IconThreeDotsC;
+export const IconPlus = IconPlusC;
+export const IconUser = IconUserC;
+export const IconReply = IconReplyC;
+export const IconShare = IconShareC;
+export const IconFilter = IconFilterC;
+export const IconSort = IconSortC;
+export const HeaderLogo = HeaderLogoC;
+export const IconComment = IconCommentC;
+export const IconImage = IconPicturesC;
+export const IconLink = ExternalLinkC;
+export const IconClose = IconCloseC;
+export const IconSmile = IconSmileC;
+export const IconGif = IconGifC;
+
+

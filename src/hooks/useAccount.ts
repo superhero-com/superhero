e5bffdb40af7b391e4e91aea574c33379c11be6d
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-08-27 08:54:34 +0100

    Refactor HeaderWalletButton to utilize useAccount hook for balance management
    - Updated HeaderWalletButton component to import and use the new useAccount hook for fetching and displaying account balance.
    - Removed unused wallet connection states and adjusted balance display logic to enhance clarity and performance.
    - Modified index file in hooks to export useAccount, ensuring better accessibility across components.
    - Implemented balance fetching logic in useAccount, improving state management and reactivity.

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,10 +1,30 @@
 import { useAtom } from "jotai";
-import { chainNamesAtom } from "../atoms/walletAtoms";
+import { balanceAtom, chainNamesAtom } from "../atoms/walletAtoms";
 import { useAeSdk } from "./useAeSdk";
+import { useEffect, useMemo } from "react";
+import { Decimal } from "../libs/decimal";
+import { toAe } from "@aeternity/aepp-sdk";
 
 export const useAccount = () => {
     const { sdk, activeAccount } = useAeSdk();
     const [chainNames, setChainNames] = useAtom(chainNamesAtom);
+    const [balance, setBalance] = useAtom(balanceAtom);
+
+    const decimalBalance = useMemo(() => Decimal.from((toAe(balance ?? 0))), [balance]);
+
+
+    async function getAccountBalance() {
+        if (!activeAccount) return;
+        const balance = await sdk?.getBalance(activeAccount);
+        setBalance(balance);
+        console.log('========================')
+        console.log('balance::', balance)
+        console.log('========================')
+    }
+
+    useEffect(() => {
+        getAccountBalance();
+    }, [activeAccount]);
 
 
 
@@ -12,6 +32,8 @@ export const useAccount = () => {
     return {
         activeAccount,
         chainNames,
+        balance,
+        decimalBalance,
     }
 };
 

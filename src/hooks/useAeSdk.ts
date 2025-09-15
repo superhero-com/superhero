26f521bb729988c6fc8f661bdf6b8f68c39ced29
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-09-15 09:44:38 +0100

    fix(#49): Update useAeSdk hook to utilize activeAccount state
    - Added activeAccount state management using Jotai for improved context handling.
    - Updated the SDK initialization logic to depend on activeAccount instead of context.activeAccount for better accuracy in SDK usage.

--- src/hooks/useAeSdk.ts
+++ src/hooks/useAeSdk.ts
@@ -1,23 +1,24 @@
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { AeSdkContext } from "../context/AeSdkProvider";
 import { WalletInfo } from "node_modules/@aeternity/aepp-sdk/es/aepp-wallet-communication/rpc/types";
 import { useAtom } from "jotai";
 import { walletInfoAtom } from "../atoms/walletAtoms";
+import { activeAccountAtom } from "@/atoms/accountAtoms";
 
 export const useAeSdk = () => {
     const [walletInfo, setWalletInfo] = useAtom<WalletInfo | undefined>(walletInfoAtom);
+    const [activeAccount, setActiveAccount] = useAtom<string | undefined>(activeAccountAtom);
 
     const context = useContext(AeSdkContext);
     if (!context) {
         throw new Error("useAeSdk must be used within an AeSdkProvider");
     }
     const sdk = useMemo(() => {
-        if (walletInfo && context.activeAccount) {
-            // if (context.aeSdk?._accounts?.current) {
+        if (walletInfo && activeAccount) {
             return context.aeSdk;
         }
         return context.staticAeSdk;
-    }, [context.aeSdk, context.staticAeSdk]);
+    }, [walletInfo, activeAccount]);
 
     return {
         ...context,

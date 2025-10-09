876e0a7a4525316c2ae2eb304047562f327bfb7a
paolomolo <paolo@finwerk.com>
2025-10-09 18:23:57 +0200

    style(social): reduce ak font size in post item
    - Change address text to text-[10px] with same line-height

--- src/features/social/components/ReplyToFeedItem.tsx
+++ src/features/social/components/ReplyToFeedItem.tsx
@@ -114,7 +114,7 @@ const ReplyToFeedItem = memo(({ item, onOpenPost, commentCount = 0 }: ReplyToFee
             </div>
             {/* On-chain link removed */}
           </div>
-          <div className="mt-1 text-[11px] text-white/65 font-mono leading-[1.2] truncate">{authorAddress}</div>
+          <div className="mt-1 text-[10px] text-white/65 font-mono leading-[1.2] truncate">{authorAddress}</div>
 
           {/* Parent context header placed under author row, before reply text */}
           {parentId && (

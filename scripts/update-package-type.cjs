85c42ec606389960c06a8f8b771031f8ae557178
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-11-06 19:49:39 +0100

    chore: update build script to include package type update and add update-package-type script

--- scripts/update-package-type.cjs
+++ scripts/update-package-type.cjs
@@ -8,6 +8,7 @@ const packageJsonPath = path.join(
   'package.json',
 );
 
+<<<<<<< HEAD
 // Read the package.json file
 fs.readFile(packageJsonPath, 'utf8', (err, data) => {
   if (err) {
@@ -40,3 +41,36 @@ fs.readFile(packageJsonPath, 'utf8', (err, data) => {
     console.log('Successfully updated package.json to use "type": "commonjs"');
   });
 });
+=======
+// Check if the package exists
+if (!fs.existsSync(packageJsonPath)) {
+  console.warn('Warning: bctsl-sdk package.json not found. Skipping package type update.');
+  process.exit(0);
+}
+
+try {
+  // Read the package.json file synchronously
+  const data = fs.readFileSync(packageJsonPath, 'utf8');
+
+  // Parse the JSON data
+  const packageJson = JSON.parse(data);
+
+  // Only update if type is "module"
+  if (packageJson.type === 'module') {
+    // Modify the "type" field
+    packageJson.type = 'commonjs';
+
+    // Convert the JSON object back to a string
+    const updatedData = JSON.stringify(packageJson, null, 2);
+
+    // Write the updated data back to the package.json file synchronously
+    fs.writeFileSync(packageJsonPath, updatedData, 'utf8');
+    console.log('Successfully updated bctsl-sdk package.json to use "type": "commonjs"');
+  } else {
+    console.log('bctsl-sdk package.json already has correct type:', packageJson.type);
+  }
+} catch (err) {
+  console.error('Error updating bctsl-sdk package.json:', err.message);
+  process.exit(1);
+}
+>>>>>>> 86e0b9e (chore: update build script to include package type update and add update-package-type script)

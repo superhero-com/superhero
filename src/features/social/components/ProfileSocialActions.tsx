5cab12b35694cf8f7effa5e8034a85ac3a02a7da
Badi Ifaoui <badi.ifaoui@zixdev.com>
2026-09-22 09:13:00 +0100

    fix(ProfileSocialActions): temporarily return null while implementing the component

--- src/features/social/components/ProfileSocialActions.tsx
+++ src/features/social/components/ProfileSocialActions.tsx
@@ -65,7 +65,8 @@ const ProfileSocialActions = ({
       )}
     </div>
   ) : null;
-
+  // temporary return null while implementing the component
+  return null;
   return (
     <>
       <div className="flex flex-1 flex-row items-center gap-2 md:flex-none">
